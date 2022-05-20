99d46ca84c4656fc5376fa73d3ea497dd6ef2248
Max <max.00745@mail.ru>
2022-05-20 10:39:47 +0300

    2PR JAVASCRIPT

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 "use strict";
 
-const a = prompt("Какую модель телефона смотрели за последнее время", ""),
-     b = +prompt ("На сколько баллов по 10-ти бальной школе оцениваете её"),
-     c = prompt("Какую модель телефона смотрели за последнее время", ""),
-     d = +prompt ("На сколько баллов по 10-ти бальной школе оцениваете её");
+// const burger = 5;
+// const fries = 0;
+// if (burger && fries){
+//      console.log("Я наелся");
+// }
+
+// console.log(burger && fries); //Это выражение вернуло нам правду.
 
 
-const databaseTelephone = {
-     manufacturedandMarks: {},
-};
-if ((b >= 1 && b <=10) && (d >= 1 && d <=10)){
-     databaseTelephone.manufacturedandMarks[a] = b;
-     databaseTelephone.manufacturedandMarks[c] = d;
-     
+
+// console.log((burger === 3 && cola && fries));
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && '121esadscads');
+// if (burger === 3 && cola === 1 && fries){
+//      console.log("Все сыты");
+// } else {
+//      console.log("Мы уходим идём в другой заведение");
+// }
+
+
+const burger = 0;
+const fries = 0;
+const cola = 0;
+if (burger || cola || fries){ //Есть хотя бы бургеры в меню
+     console.log("Все сыты");
+} else {
+     console.log("Мы уходим идём в другой заведение");
 }
-else {
-     alert("Ошибка,впишите значения по 10-ти бальной школе");
-}
-console.log(databaseTelephone);
+ 
+console.log (burger || cola || fries);
+
+let johnReport, alexReport, samReport, mariaReport = "done";
+
+console.log(johnReport || alexReport || samReport || mariaReport)
